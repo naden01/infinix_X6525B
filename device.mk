77ce8bc7f574469aabbad4235afa3073609eb5e3
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-11-14 16:39:10 +0700

    manifest
    11-13 08:15:52.474   376   376 E HidlServiceManagement: getService: Potential race detected. The VINTF manifest is not being enforced. If a HAL server has a delay in starting and it is not in the manifest, it will not be retrieved. Please make sure all HALs on this device are in the VINTF manifest and enable PRODUCT_ENFORCE_VINTF_MANIFEST on this device (this is also enabled by PRODUCT_FULL_TREBLE). PRODUCT_ENFORCE_VINTF_MANIFEST will ensure that no race condition is possible here.
    11-13 08:15:52.685   400   400 E cutils-trace: Error opening trace file: No such file or directory (2)

--- device.mk
+++ device.mk
@@ -38,6 +38,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# manifest
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
