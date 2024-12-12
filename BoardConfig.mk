e0b39de598fe77c57eba13d74485fa5ab083eb6d
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-12-12 09:18:50 -0800

    keymaster thingy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,12 +148,17 @@ TW_INCLUDE_FBE_METADATA_DECRYPT := true
 TW_FORCE_KEYMASTER_VER          := true
 
 # Additional binaries & libraries needed for recovery
+
 TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hardware.keymaster@4.1 \
     libkeymaster4 \
+    libkeymaster41 \
     libpuresoftkeymasterdevice
 
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1 \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # Excludes
